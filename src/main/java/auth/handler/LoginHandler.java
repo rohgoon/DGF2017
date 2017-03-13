b77bfb7440eaadb62c6865ccbc6ad9293230f84a
NCG <NCG@DESKTOP-T6ENAEQ>
2017-03-13 12:18:20 +0900

    수정

--- src/main/java/auth/handler/LoginHandler.java
+++ src/main/java/auth/handler/LoginHandler.java
@@ -16,26 +16,7 @@ public class LoginHandler implements CommandHandler {
 		}else if (req.getMethod().equalsIgnoreCase("post")) {
 			String id = req.getParameter("id");
 			String password = req.getParameter("password");
-			Connection conn =null;
 			
-			try{
-			/*	conn = ConnectionProvider.getConnection();
-				Member member = dao.selectAllById(conn, id);
-				if (member == null) {
-					req.setAttribute("notJoin", true);
-					return FORM_VIEW;
-				}
-				if (!member.matchPassword(password)) {
-					req.setAttribute("idOrPwdNotMatch", true);
-					return FORM_VIEW;
-				}
-				User user =new User(id, member.getName());
-				req.getSession().setAttribute("auth", user);
-				return "index.jsp";
-				*/
-			}finally {
-				//JdbcUtil.close(conn);
-			}
 		}
 		return null;
 	}
