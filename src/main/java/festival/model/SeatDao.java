b6dff4cd1f5167b8a90a8845bc584a196e60b34c
NCG <NCG@DESKTOP-T6ENAEQ>
2017-03-16 09:48:09 +0900

    session NullPointException 오류 해결. 오타나 뷰테이블 맵핑문제로 확인

--- src/main/java/festival/model/SeatDao.java
+++ src/main/java/festival/model/SeatDao.java
@@ -6,7 +6,7 @@ import java.util.List;
 public interface SeatDao {
 	public void insert(Seat seat) throws SQLException;
 	public Seat selectBySno(int sno) throws SQLException;
-	public Seat selectByDnoAnoGrade(Seat seat) throws SQLException;
+	public Seat selectByDnoAndGrade(Seat seat) throws SQLException;
 	public List<Seat> selectListByDno(int dno) throws SQLException;
 	public List<Seat> selectListAll() throws SQLException;
 	public void updateByDnoAndGrade(Seat seat) throws SQLException;
--- src/main/java/reservation/model/ReservationDao.java
+++ src/main/java/reservation/model/ReservationDao.java
@@ -7,7 +7,7 @@ public interface ReservationDao {
 	public void insert(Reservation reservation) throws SQLException;
 	public List<Reservation> selectAllbyUno(int uno) throws SQLException;
 	public List<Reservation> selectList() throws SQLException;
-	public void updateSnoByRno(Reservation reservation) throws SQLException;
+	public void updateSnoAndRtimeByRno(Reservation reservation) throws SQLException;
 	public List<ReservationView> selectDetailListbyUno(int uno) throws SQLException;
 	public ReservationView selectDetailByRno(int rno) throws SQLException;
 }
