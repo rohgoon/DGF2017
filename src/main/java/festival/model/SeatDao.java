09bc7d6baf875b6932e948e94935860daae6e95c
NCG <NCG@DESKTOP-T6ENAEQ>
2017-03-16 09:39:53 +0900

    수정

--- src/main/java/festival/model/SeatDao.java
+++ src/main/java/festival/model/SeatDao.java
@@ -6,11 +6,11 @@ import java.util.List;
 public interface SeatDao {
 	public void insert(Seat seat) throws SQLException;
 	public Seat selectBySno(int sno) throws SQLException;
-	public Seat selectByDnoAnoGrade(Seat seat) throws SQLException;
+	public Seat selectByDnoAndGrade(Seat seat) throws SQLException;
 	public List<Seat> selectListByDno(int dno) throws SQLException;
 	public List<Seat> selectListAll() throws SQLException;
 	public void updateByDnoAndGrade(Seat seat) throws SQLException;
 	public void updateBySno(Seat seat) throws SQLException;
 	public Seat selectMaxAndSoldBySno(int sno) throws SQLException;
-	public void updateSoldBySno(int sno) throws SQLException;
+	public void updateSoldBySno(Seat seat) throws SQLException;
 }
