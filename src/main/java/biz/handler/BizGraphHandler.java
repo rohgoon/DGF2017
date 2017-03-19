facd2fdc324033d9bfe361ca2b070096f38e07f5
rohgoon <rohgoon@PTAHKA>
2017-03-19 14:13:47 +0900

    properties 및 handler 생성

---
+++ src/main/java/biz/handler/BizGraphHandler.java
@@ -0,0 +1,26 @@
+package biz.handler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.ibatis.session.SqlSession;
+
+import auth.model.LoginUser;
+import member.model.MemberDao;
+import member.model.User;
+import mvc.controller.CommandHandler;
+import mvc.util.MySqlSessionFactory;
+
+public class BizGraphHandler implements CommandHandler {
+	private final String FORM_VIEW = "/WEB-INF/view/login.jsp";
+	@Override
+	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		if (req.getMethod().equalsIgnoreCase("get")) {
+			return FORM_VIEW;
+		}else if (req.getMethod().equalsIgnoreCase("post")) {
+			
+		}
+		return null;
+	}
+
+}
