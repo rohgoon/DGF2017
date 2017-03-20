1d2b19c865948e956c8d8677c1633d90569459b7
NCG <NCG@DESKTOP-T6ENAEQ>
2017-03-20 09:36:19 +0900

    SeatPerGrade 생성

---
+++ src/main/java/biz/model/SeatPerGrade.java
@@ -0,0 +1,5 @@
+package biz.model;
+
+public class SeatPerGrade {
+	
+}
--- src/main/java/biz/model/bmFestival.java
+++ src/main/java/biz/model/bmFestival.java
@@ -1,17 +1,19 @@
 package biz.model;
 
+import java.util.HashMap;
+
 public class bmFestival {
 	private int fno;
 	private String day;
-	private String[][] soldPerGrade;
-	private String[][] pricePerGrade;
+	private HashMap<String, SeatPerGrade> soldPerGrade;
+	private HashMap<String, SeatPerGrade> pricePerGrade;
 	private int totalTicket;
 	private int totalPrice;
 	public bmFestival() {
 		super();
 	}
-	public bmFestival(int fno, String day, String[][] soldPerGrade, String[][] pricePerGrade, int totalTicket,
-			int totalPrice) {
+	public bmFestival(int fno, String day, HashMap<String, SeatPerGrade> soldPerGrade,
+			HashMap<String, SeatPerGrade> pricePerGrade, int totalTicket, int totalPrice) {
 		super();
 		this.fno = fno;
 		this.day = day;
@@ -32,16 +34,16 @@ public class bmFestival {
 	public void setDay(String day) {
 		this.day = day;
 	}
-	public String[][] getSoldPerGrade() {
+	public HashMap<String, SeatPerGrade> getSoldPerGrade() {
 		return soldPerGrade;
 	}
-	public void setSoldPerGrade(String[][] soldPerGrade) {
+	public void setSoldPerGrade(HashMap<String, SeatPerGrade> soldPerGrade) {
 		this.soldPerGrade = soldPerGrade;
 	}
-	public String[][] getPricePerGrade() {
+	public HashMap<String, SeatPerGrade> getPricePerGrade() {
 		return pricePerGrade;
 	}
-	public void setPricePerGrade(String[][] pricePerGrade) {
+	public void setPricePerGrade(HashMap<String, SeatPerGrade> pricePerGrade) {
 		this.pricePerGrade = pricePerGrade;
 	}
 	public int getTotalTicket() {
@@ -56,6 +58,5 @@ public class bmFestival {
 	public void setTotalPrice(int totalPrice) {
 		this.totalPrice = totalPrice;
 	}
-	
 	
 }
