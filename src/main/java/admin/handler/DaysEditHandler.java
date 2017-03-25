7b2cd58f1f93257927cb81b8054f913a34d5d120
NCG <NCG@DESKTOP-T6ENAEQ>
2017-03-25 16:52:56 +0900

    DaysEditHandler 제작중. Del은 정학씨 자료와 merge후 진행

--- src/main/java/admin/handler/DaysEditHandler.java
+++ src/main/java/admin/handler/DaysEditHandler.java
@@ -5,19 +5,35 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.ibatis.session.SqlSession;
 
-import member.model.MemberDao;
+import festival.model.Days;
+import festival.model.DaysDao;
 import mvc.controller.CommandHandler;
 import mvc.util.MySqlSessionFactory;
 
 public class DaysEditHandler implements CommandHandler {
-
+	
 	@Override
 	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
-		if (req.getMethod().equalsIgnoreCase("get")) {			
-			return "/WEB-INF/admin/adminFestivalForm.jsp";
+		if (req.getMethod().equalsIgnoreCase("get")) {
+			int dno = Integer.parseInt(req.getParameter("dno"));
+			SqlSession session = null;
+			try {
+				session = MySqlSessionFactory.openSession();
+				DaysDao daysDao = session.getMapper(DaysDao.class);
+				Days days = daysDao.selectByDno(dno);
+				req.setAttribute("days", days);
+				
+
+			} finally {
+				session.close();
+			}
+			
+			return "/WEB-INF/admin/afInfoDays.jsp";
 		}else if (req.getMethod().equalsIgnoreCase("post")) {
 			
-			return "/WEB-INF/admin/adminFestivalSuccess.jsp";
+			
+			//fno 바로 넘겨주는지 확인
+			return "afInfo.do";
 		}
 		return null;
 	}
